8813655a08cbaa430138e73d01cb3649c753108c
Vitaliy V. Shopov <unitymind.apps@gmail.com>
2011-04-02 14:59:49 +0700

    Initial\ commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TimeControl::Application.config.secret_token = 'c557ee1e801a7c9e5ef15455e0c9fbe28d86bd129543cd24f0892f41d211e473784c0c7f82e3107cbea4794e958423e3cf4b4908751553742fcddccf91ab4201'
